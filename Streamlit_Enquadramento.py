c5fcfc87e7e89dbfceacd71dbd035c62d63a9557
Juan Carneiro <you@example.com>
2025-09-18 16:05:41 -0300

    codigo unificado

--- Streamlit_Enquadramento.py
+++ Streamlit_Enquadramento.py
@@ -100,7 +100,7 @@ def run():
     with st.container():
         cols = st.columns([0.095, 0.905])
         with cols[0]:
-            st.image("imagens/Capital-branca.png", width=400, output_format="PNG")
+            st.image("Imagens/Capital-branca.png", width=400)
         with cols[1]:
             st.markdown(
                 f"""
